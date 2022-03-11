e5e9f0a1fcd319cce77b6786dbd00972e084f813
AlexRamosGlz <joralexfx@gmail.com>
2022-03-11 10:22:36 -0600

    Comments Added

--- CodingChallenge_1.py
+++ CodingChallenge_1.py
@@ -1,7 +1,14 @@
+
+# Decclaring a variable with the message as a value
 message = 'Hello there!, General Kenoby!';
+
+#this two variables will be the numbers to add at the third variable 
 num1 = 52;
 num2 = 36;
 
+#the variable that sums both numbers declared above
 sum = num1 + num2;
 
+#this mf prints both the message and the result of the sum
 print("This is the message: ",message, "\nthis is the result of the sum: ",sum);
+
---
+++ CodingChallenge_2.py
@@ -0,0 +1,8 @@
+from email import message
+
+
+def String101(message1, message2):
+  return message1 + message2;
+
+print(String101('Hello there!', "\nGeneral Kenoby!"));
+
