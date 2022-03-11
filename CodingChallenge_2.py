5e3e219e2fbb8b1ae93743d587e28e4c6d7b0ebb
AlexRamosGlz <joralexfx@gmail.com>
2022-03-11 10:47:39 -0600

    Comments Added

--- CodingChallenge_2.py
+++ CodingChallenge_2.py
@@ -1,8 +1,8 @@
-from email import message
 
-
+#this functions takes 2 string and then return the 2 strings concatenated
 def String101(message1, message2):
   return message1 + message2;
 
+#this fella calls the functions and then prints the returned value
 print(String101('Hello there!', "\nGeneral Kenoby!"));
 
