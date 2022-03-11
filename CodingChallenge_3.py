c89d8107cde8e03d508c0b9c4e4ea6fe7cd0547e
AlexRamosGlz <joralexfx@gmail.com>
2022-03-11 12:06:47 -0600

    Coding Challenge Resolved

---
+++ CodingChallenge_3.py
@@ -0,0 +1,16 @@
+
+#the function will concat the 2 strings
+def concatString(name, lastName):
+
+  #this 2 variables will stored the value from the parameters
+  name = name;
+  lastName = lastName;
+
+  #the function returns the 2 strings concatenated
+  return name + ' ' + lastName;
+
+#as usual this mf prints the value returned
+print(concatString('Jorge', 'Ramos Gonzalez'));
+
+#and this other fella prints the 2 concatenated numbers
+print(10, 10);
