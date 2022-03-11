bd22829cc90c70c277dda7a84b27986d0d37be9a
AlexRamosGlz <joralexfx@gmail.com>
2022-03-11 11:13:23 -0600

    coding fixed

--- CodingChallenge_2.py
+++ CodingChallenge_2.py
@@ -1,7 +1,18 @@
 
 #this functions takes 2 string and then return the 2 strings concatenated
 def String101(message1, message2):
-  return message1 + message2;
+
+  #the parameter values will be stored in this var 
+  completeMessage =  message1 + message2;
+
+  #and then print it 
+  print(completeMessage);
+
+  #the phrase is declared inside the function 
+  phrase = '"print()" se utiliza para imprimir valores en la consola.';
+
+  #the same phrase will be the value returned from the function
+  return phrase;
 
 #this fella calls the functions and then prints the returned value
 print(String101('Hello there!', "\nGeneral Kenoby!"));
