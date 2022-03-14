b48f64910e5e9f695a769baead8e861cac7f3747
AlexRamosGlz <joralexfx@gmail.com>
2022-03-14 17:46:15 -0600

    Coding Challenge resolved

---
+++ CodingChallenge_11 & 12.py
@@ -0,0 +1,6 @@
+def removeFromList(lista): 
+  lista.remove('amarillo');
+  lista.remove('rojo');
+  return lista;
+
+print(removeFromList(['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']));
