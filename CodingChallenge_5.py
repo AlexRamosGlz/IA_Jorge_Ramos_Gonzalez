75869a1a2a6928314950bbd09955b00b5cfb1631
AlexRamosGlz <joralexfx@gmail.com>
2022-03-14 12:00:03 -0600

    Coding Challenge Resolved

---
+++ CodingChallenge_5.py
@@ -0,0 +1,7 @@
+
+#this functions format the string
+def tabString():
+  return '\n\t- Python.\n\t- JavaScript.\n\t- Java.\n\t- PHP.\n\t- C#.\n\t- Ruby.'
+
+#printing the return value
+print(tabString());
