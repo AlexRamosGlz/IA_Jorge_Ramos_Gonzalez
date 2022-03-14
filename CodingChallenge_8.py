a3f0e3355714f36edbe9ed25745a9bc0f1bb2b7e
AlexRamosGlz <joralexfx@gmail.com>
2022-03-14 16:47:35 -0600

    coding Challenge resolved

---
+++ CodingChallenge_8.py
@@ -0,0 +1,9 @@
+
+#declared the function that rounds the given number
+def roundNumber(number): 
+  
+  #return the given number rounded for 5 decimals
+  return round(number, 5);
+
+#print the returned value
+print(roundNumber(17.567383292929200234));
