012206506a308aecac39d1cbd5afc622df37cb0c
AlexRamosGlz <joralexfx@gmail.com>
2022-03-14 12:24:02 -0600

    Coding Challenge Resolved

---
+++ CodingChallenge_6.py
@@ -0,0 +1,21 @@
+def calculator():
+
+  print('Los Resultados son: ')
+
+  suma = 20 + 23 + 44;
+  print('\nSuma: ', suma)
+
+  resta = 20 - 23 - 84;
+  print('\nResta: ', resta)
+
+  multiplicacion = (20 + 23 + 44) * 10;
+  print('\nMultiplicacion: ', multiplicacion);
+
+  division = 5000 / (230 + 250) ;
+  print('\nDivision: ', division);
+
+
+  return 'Return: ', (10 / 5) + 15 - 17; 
+  
+
+print(calculator());
