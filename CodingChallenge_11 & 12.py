9606cac955b04f8b61f853fec4cb3f9cbafe5b09
AlexRamosGlz <joralexfx@gmail.com>
2022-03-15 10:22:07 -0600

    Update CodingChallenge_11 & 12.py

--- CodingChallenge_11 & 12.py
+++ CodingChallenge_11 & 12.py
@@ -1,6 +1,13 @@
+
+#thi function removes two elements
 def removeFromList(lista): 
+
+  #elements to remove
   lista.remove('amarillo');
   lista.remove('rojo');
+
+  #returning the lista
   return lista;
 
+#printing the returned value
 print(removeFromList(['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']));
