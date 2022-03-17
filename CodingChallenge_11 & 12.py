51c2a6af7d2840f48f15e5a461de5f52d5a1c7eb
AlexRamosGlz <joralexfx@gmail.com>
2022-03-16 20:34:55 -0600

    Update CodingChallenge_11 & 12.py

--- CodingChallenge_11 & 12.py
+++ CodingChallenge_11 & 12.py
@@ -11,3 +11,10 @@ def removeFromList(lista):
 
 #printing the returned value
 print(removeFromList(['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']));
+
+
+def popElement(lista):
+  newLista = [lista.pop(lista.index('azul')), lista.pop(lista.index('blanco'))];
+  return newLista
+
+print('Poped Elements: ',popElement(['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']));
