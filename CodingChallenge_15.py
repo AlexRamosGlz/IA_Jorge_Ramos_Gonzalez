f781f874240d5137b764436ebdcd082e5ad6c6b0
AlexRamosGlz <joralexfx@gmail.com>
2022-03-18 10:49:19 -0600

    Coding Challenge Resolved

---
+++ CodingChallenge_15.py
@@ -0,0 +1,9 @@
+
+#this function sorts the given list
+def sortLista(lista):
+
+  #returning the list but sorted
+  return lista.sort()
+
+#printing the returned value from the function above 
+print('Lista Sorted: ', sortLista(['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']))
