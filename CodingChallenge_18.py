adcbc6d187da8ecccc31276b52d3b77b0f4ea4f8
AlexRamosGlz <joralexfx@gmail.com>
2022-03-18 17:38:34 -0600

    Coding Challenge Resolved

---
+++ CodingChallenge_18.py
@@ -0,0 +1,33 @@
+
+def isDifferent():
+  num1 = 15
+  num2 = 20
+
+  if num1 != num2:
+    print('is Diferent', num1 != num2)
+  
+
+isDifferent()
+
+def isGreater():
+  num1 = 1450 
+  num2 = 60
+
+  if num1 > num2:
+	  print('is greater: ', num1 > num2)
+
+isGreater()
+
+
+def isEqual():
+  num1 = 1450
+  num2 = 60
+
+  if num1 != num2:
+	  print('Is Equal: ', num1 != num2)
+
+isEqual()
+
+
+
+  
