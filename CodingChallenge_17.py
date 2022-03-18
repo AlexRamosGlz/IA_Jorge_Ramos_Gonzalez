cf136da528b31524c0e05765187c91b8746d7543
AlexRamosGlz <joralexfx@gmail.com>
2022-03-18 11:34:50 -0600

    Coding Challenge Resolved

---
+++ CodingChallenge_17.py
@@ -0,0 +1,9 @@
+
+#this functions receives a list and then convert it into a tuple
+def tupleConverter(list):
+
+  #returns the list as a tuple
+  return tuple(list)
+
+#prints whether or not is a tuple 
+print(type(tupleConverter(['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'])));
