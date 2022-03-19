6297458550c21981244338a318227aacb986983b
AlexRamosGlz <joralexfx@gmail.com>
2022-03-18 23:49:47 -0600

    Coding Challenge Resolved

---
+++ CodingChallenge_19.py
@@ -0,0 +1,13 @@
+
+#this functions checks whether is red or not
+def isOlderEnough(color):
+
+  #if color is not red then print 'is not red', else print 'is red'
+  if color != 'rojo':
+    print("El color no es rojo")
+  else:
+    print('EL color es rojo')
+
+#calling the functions 
+isOlderEnough('azul')
+
