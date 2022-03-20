916adba88846ffabed1bd2d250ad6600669499dc
AlexRamosGlz <joralexfx@gmail.com>
2022-03-19 22:39:17 -0600

    Coding Challenge Resolved

---
+++ CodingChallenge_26.py
@@ -0,0 +1,14 @@
+
+#this function sum 2 numbers
+def sum(num1, num2):
+
+  #printing the result
+  return print('The result of the sum is: ', (num1 + num2))
+
+
+
+#calling the function
+sum(15, 15)
+sum(25, 25)
+sum(50000, 7000)
+
