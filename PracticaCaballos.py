396c2f0ee4ff96d29c44e0529a105458f8b67490
AlexRamosGlz <joralexfx@gmail.com>
2022-05-11 17:37:26 -0500

    Update PracticaCaballos.py

--- PracticaCaballos.py
+++ PracticaCaballos.py
@@ -1,5 +1,5 @@
 import random
-
+import cv2
 
 a = [0, 0, 0, 0, 0]
 b = [0, 0, 0, 0, 0]
@@ -54,3 +54,43 @@ while(stop == False):
 print('\nSe nececistan minimo: ', counter, 'carreras')
 
 
+def printGrafo(caballos):
+  #Initialice 
+  grafo = 255*np.ones((250,550,3),dtype=np.uint8)
+  font = cv2.FONT_HERSHEY_TRIPLEX
+
+  # Creación de los nodos del grafo
+  #Primer lugar
+  cv2.circle(grafo,(250,50),10,(0,0,0),-1)
+  cv2.putText(grafo,'1er: ' + str(VelCaballos[4,4]),(280,50),font,1,(0,0,0),2,cv2.LINE_AA)
+
+  #Segundo lugar
+  cv2.circle(grafo,(250,100),10,(0,0,0),-1)
+  cv2.putText(grafo,'2do: ' + str(VelCaballos[3,4]),(280,100),font,1,(0,0,0),2,cv2.LINE_AA)
+  cv2.line(grafo,(250,100),(250,50),(255,0,0),2)
+
+  #Lugares extra 
+  cv2.circle(grafo,(125,150),10,(0,0,0),-1)
+  cv2.putText(grafo,'3er',(150,150),font,1,(0,0,0),2,cv2.LINE_AA)
+  cv2.line(grafo,(125,150),(250,150),(255,0,0),2)
+
+  cv2.circle(grafo,(375,150),10,(0,0,0),-1)
+  cv2.putText(grafo,'4to',(395,150),font,1,(0,0,0),2,cv2.LINE_AA)
+  cv2.line(grafo,(375,150),(250,150),(255,0,0),2)
+
+  cv2.circle(grafo,(125,200),10,(0,0,0),-1)
+  cv2.putText(grafo,'5to',(150,200),font,1,(0,0,0),2,cv2.LINE_AA)
+  cv2.line(grafo,(125,200),(250,200),(255,0,0),2)
+
+  cv2.circle(grafo,(375,200),10,(0,0,0),-1)
+  cv2.putText(grafo,'6to',(395,200),font,1,(0,0,0),2,cv2.LINE_AA)
+  cv2.line(grafo,(375,200),(250,200),(255,0,0),2)
+
+  #Conexiones extras
+  cv2.line(grafo,(250,150),(250,100),(255,0,0),2)
+  cv2.line(grafo,(250,200),(250,150),(255,0,0),2)
+
+  #Impresión del grafo
+  cv2.imshow('Grafo',grafo)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
